a7784cdf2c1325499bc98272b1382a1e9a3aab92
Kate Huggins <khuggins22@gmail.com>
2018-03-26 19:43:29 -0400

    3.9 layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'simple_pages/what'
+
+  get 'simple_pages/coaching'
+
+  get 'simple_pages/login'
+
   get 'simple_pages/about'
 
   get 'simple_pages/contact'
