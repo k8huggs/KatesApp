3a3168f77492c0a381379dec5ae4d9c733d72c31
Kate Huggins <khuggins22@gmail.com>
2018-05-22 13:46:49 -0400

    5.2 search bar revision

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,10 @@
 class Product < ApplicationRecord
-  has_many :orders
-end
+    has_many :orders
 
-def self.search(search_term)
-  Product.where("name LIKE ?", "%#{search_term}%")
+    def self.search(search_term)
+      if Rails.env.production?
+        Product.where("name ilike ?", "%#{search_term}%")
+      else
+        Product.where("name LIKE ?", "%#{search_term}")
+    end
 end
