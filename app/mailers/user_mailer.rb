a52bf7857d66542d551c07a2d6e22e89ad58ee83
Kate Huggins <khuggins22@gmail.com>
2018-06-17 17:59:43 -0400

    5.11 mailer & styling

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,9 @@ class UserMailer < ApplicationMailer
           to: 'khuggins22@gmail.com',
           subject: "A new contact form message from #{name}")
   end
+
+  def welcome (user)
+    @appname = "Transience"
+    mail(to: user.email,
+          subject: "Welcome to #{@appname}! Get ready to meet your coach!")
 end
