ceb8af3dedc25f19a91e6aeefd1967c2980ae73a
Kate Huggins <khuggins22@gmail.com>
2018-07-17 16:56:12 -0400

    6.3 rspec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -15,13 +15,19 @@ describe UsersController, type: :controller do
 
     context 'when a user is logged in' do
       before do
-        sign_in :user
+        sign_in user
       end
       it 'loads correct user details' do
         get :show, params: { id: user.id }
         expect(assigns(:user)).to eq user
-        expect(response).to have_http_status(200)
+        # expect(response).to have_http_status(200)
       end
+    end
+
+    it 'tries to access show page of the second user' do
+      get :show, params: {id: user2.id}
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to(root_path)
     end
 
     context 'when a user is not logged in' do
