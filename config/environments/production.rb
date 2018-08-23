53d71a89ef8a66415c6b4176792cd9fb4f4d49b7
Kate Huggins <khuggins22@gmail.com>
2018-08-23 12:14:38 -0400

    6.13 Action Cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,4 +104,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://transiencellc.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://transiencellc.herokuapp.com', 'https://transiencellc.herokuapp.com']
 end
