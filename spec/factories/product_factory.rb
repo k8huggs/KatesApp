8e2a28c6feb05509d1847638b1f2579d7465accd
Kate Huggins <khuggins22@gmail.com>
2018-08-26 21:58:43 -0400

    6.15 Tests Added

---
+++ spec/factories/product_factory.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+    sequence(:name) { |n| "product#{n}" }
+   	factory :product, class: Product do
+    	name
+    	description "30 Minute Midgrade"
+    	price 45.00
+    	color "Pretty beautiful"
+    	image_url "app/assets/images/spotify.jpg"
+    end
+  end
