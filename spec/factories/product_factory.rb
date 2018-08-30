aa58a56ff1acb528262ba5ee8fce14efe50130c3
Kate Huggins <khuggins22@gmail.com>
2018-08-30 15:25:13 -0400

    DELETED ALL to try to FIX this thing

--- spec/factories/product_factory.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :product, class: Product do
-    name "test product"
-    description "test description"
-    image_url "test.jpg"
-    colour "blue"
-    price "12.5"
-  end
-end
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,19 +1,1 @@
-FactoryBot.define do
-  sequence(:email) {|n| "user#{n}@example.com"}
 
-  factory :user, class: User do
-    email
-    password "testpassword"
-    first_name "Peter"
-    last_name "Tester"
-    admin false
-  end
-
-  factory :admin, class: User do
-    email "admin@example.de"
-    password "testpassword"
-    first_name "real"
-    last_name "admin"
-    admin true
-  end
-end
