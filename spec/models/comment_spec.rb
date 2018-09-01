a31f32b8da13ee6eee6752f4c7cbd37b03f4f134
Kate Huggins <khuggins22@gmail.com>
2018-08-31 22:22:06 -0400

    6.15 spec/models/comment_spec.rb

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 describe Comment do
   context "if product has comments" do
-    product_category = Category.create!(
-      product_category: "Test Category-Comments",
-      description: "A test category for Comments",
-      image_url: "products/p1.jpg"
-    )
+    # product_category = Category.create!(
+    #   product_category: "Test Category-Comments",
+    #   description: "A test category for Comments",
+    #   image_url: "products/p1.jpg"
+    # )
     let(:product) {
       Product.create!(
         name: "Product-test",
